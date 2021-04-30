eeaf825bf112e7bfdd82f3dcc8e4e793802f79b7
luismartinezs <martinez.s.luis@gmail.com>
2021-04-30 14:12:43 +0200

    api with loading time

--- src/api/event.js
+++ src/api/event.js
@@ -1,6 +1,10 @@
-// dumb mock api
-export const getEventCount = value => {
-  return Promise.resolve(JSON.stringify(value.length))
+let count = 0
+
+export const getEventCount = () => {
+  return new Promise(resolve => {
+    count++
+    return setTimeout(() => resolve(count), 300)
+  })
 }
 
 export default { getEventCount }
